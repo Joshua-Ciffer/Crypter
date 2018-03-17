0c2ae079fbd11da1629622c4fdad87a167f6c593
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-03-17 16:56:11 -0400

    Started parse arguments functions.

--- batch/Crypter.bat
+++ batch/Crypter.bat
@@ -2,11 +2,47 @@
 setlocal
 :: Author: Joshua Ciffer
 :: Version: 03/17/2018
+:: This file is the main command that calls subscripts and functions for the Crypter program.
 
+:: Program version.
 set version=3.17.18
-title Crypter %version%
 
-:Help
+:: The name of this file.
+set parent=%0
+
+:: The command to execute.
+set command=%1
+
+:: Options and command arguments.
+set arg1=%2
+set arg2=%3
+set arg3=%4
+set arg4=%5
+set arg5=%6
+set arg6=%7
+set arg7=%8
+set arg8=%9
+
+echo arg0=%parent% arg1=%command% arg2=%arg1% arg3=%arg2% arg4=%arg3% arg5=%arg4% arg6=%arg5% arg7=%arg6% arg8=%arg7% arg9=%arg8%
+	
+:: Determines command behavior based on what command line arguments were passed.
+:parseArgs
+	if "%command%"=="" (
+		call :help
+	) else if /i "%command%"=="help" (
+		call :help
+	) else if /i "%command%"=="encrypt" (
+		call encrypt.bat %arg1% %arg2% %arg3% %arg4% %arg5% %arg6% %arg7% %arg8%
+	) else if /i "%command%"=="decrypt" (
+		call decrypt.bat %arg1% %arg2% %arg3% %arg4% %arg5% %arg6% %arg7% %arg8%
+	) else (
+		echo.
+		echo Unrecognized command.
+		call :help
+	)
+	goto:eof
+	
+:help
 	echo.
 	echo Usage: Crypter ^<command^> [^<options^>] [args]
 	echo.
