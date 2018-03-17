8112970b5b208de301ff4574ac9be28599220802
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-03-17 18:25:20 -0400

    Worked on Encrypt commind argument selection.

--- batch/Crypter.bat
+++ batch/Crypter.bat
@@ -27,12 +27,12 @@ echo arg0=%parent% arg1=%command% arg2=%arg1% arg3=%arg2% arg4=%arg3% arg5=%arg4
 	
 :: Determines command behavior based on what command line arguments were passed.
 :parseArgs
-	if "%command%"=="" (
+	if /i "%command%"=="" (
 		call :help
 	) else if /i "%command%"=="help" (
 		call :help
 	) else if /i "%command%"=="encrypt" (
-		call encrypt.bat %arg1% %arg2% %arg3% %arg4% %arg5% %arg6% %arg7% %arg8%
+		call encrypt.bat %arg1% %arg2% %arg3% %arg4% %arg5%
 	) else if /i "%command%"=="decrypt" (
 		call decrypt.bat %arg1% %arg2% %arg3% %arg4% %arg5% %arg6% %arg7% %arg8%
 	) else (
@@ -40,7 +40,7 @@ echo arg0=%parent% arg1=%command% arg2=%arg1% arg3=%arg2% arg4=%arg3% arg5=%arg4
 		echo Unrecognized command.
 		call :help
 	)
-	goto:eof
+	goto :eof
 	
 :help
 	echo.
@@ -48,21 +48,13 @@ echo arg0=%parent% arg1=%command% arg2=%arg1% arg3=%arg2% arg4=%arg3% arg5=%arg4
 	echo.
 	echo Commands:
 	echo 	Encrypt
-	echo 		Usage: Crypter Encrypt [^<options^>] [args]		
-	echo 		Options:
-	echo 			-f 	Encrypts a text file.
-	echo 			-s 	Saves encrypted output to a specefied file.
-	echo 			-h 	Displays command usage help.
 	echo 	Decrypt
-	echo 		Usage: Crypter Decrypt [^<options^>] [args]
-	echo 		Options:
-	echo 			-h 	Displays command usage help.
-	echo 	Help	Displays information about command usage.
-	echo 		Usage: Crypter Help
+	echo 	Help
+	echo.
 	echo About
 	echo 	Written by: Joshua Ciffer
 	echo 	Version: %version%
 	echo 	Source Code: https://github.com/Joshua-Ciffer/Crypter
-	goto:eof
+	goto :eof
 
 endlocal
--- batch/Encrypt.bat
+++ batch/Encrypt.bat
@@ -4,32 +4,91 @@ setlocal
 :: Version: 03/17/2018
 :: This file executes commands to encrypt text.
 
-:: 
+echo Inside encrypt.bat
+
+:: Options and command line arguments.
 set arg1=%1
 set arg2=%2
 set arg3=%3
 set arg4=%4
 set arg5=%5
-set arg6=%6
-set arg7=%7
-set arg8=%8
-set arg9=%9
+
+:: The method of text input.  Determined by command argument.
+:: -string sets inputMode=string
+:: -file sets inputMode=file
+set inputMode=
+
+:: Determines whether or not encrypted output will be saved to a file.  Determined by command argument.
+:: -save sets saveOutput=true
+:: No argument sets saveOutput=false
+set saveOutput=
+
+:: The path of the input file.  Only used if -file is used.
+set inputPath=
+
+:: The path of the output file.  Only used if -save is used.
+set outputPath=
+
+set error=
 
 :: Determines command behavior based on what command line arguments were passed.
-:parseArgs
-	echo arg0=%0 arg1=%1 arg2=%2 arg3=%3 arg4=%4 arg5=%5 arg6=%6 arg7=%7 arg8=%8 arg9=%9
+:parseArgs (
+	echo arg0=%0 arg1=%1 arg2=%2 arg3=%3 arg4=%4
+	echo test4
+	if /i "%arg1%"=="" (
+		call :help
+		echo test3
+	) else if /i "%arg1%"=="-help" (
+		if /i "%arg2%"=="" (
+			call :help
+		) else (
+			call :incorrectOptionUse
+		)
+		echo test1
+	) else if /i "%arg1%"=="-string" (
+		set inputMode=string
+		if /i "%arg2%"=="-save" (
+			if /i "%arg4%"="" (
+				set saveOutput=true
+				set outputPath=%~f3
+				:: Encrypt
+			) else (
+				call :incorrectOptionUse
+			)
+		) else (
+			if /i "%arg2%"=="" (
+				set saveOutput=false
+				:: Encrypt
+			) else (
+				call :incorrectOptionUse
+			)
+		)
+		echo test2
+	) else if /i "%arg1%"=="-file" (
+		set inputMode=file
+		set inputPath=%~f2
+		if /i "%arg3%"=="-save" (
+			if /i "%arg5%"="" (
+				set saveOutput=true
+				set outputPath=%~f4
+				:: Encrypt
+			) else (
+				call :incorrectOptionUse
+			)
+		) else (
+			if /i "%arg%3"=="" (
+				set saveOutput=false
+				:: Encrypt
+			) else (
+				call :incorrectOptionUse
+			)
+		)
+	) else (
+		call :incorrectOptionUse
+	)
+	goto :eof
+)
 
-
-
-
-
-
-
-
-
-
-
-	goto:eof
 
 :TextEncryption
 	(set CHAR[a]=UDFM45) & (set CHAR[b]=H21DGF) & (set CHAR[c]=FDH56D) & (set CHAR[d]=FGS546) & (set CHAR[e]=JUK4JH)
@@ -76,3 +135,33 @@ set arg9=%9
 	echo.
 	pause
 	goto :TextEncryption
+
+:incorrectOptionUse
+	echo.
+	echo Incorrect usage of options.  Please refer to the command usage.
+	call :help
+	goto :eof
+	
+:help
+	echo.
+	echo Usage: Crypter Encrypt [-help] ^| [-string ^| [-file input-path]] [-save output-path]
+	echo.
+	echo Options:
+	echo 	-help 		Displays command usage help.
+	echo 	-string 	Encrypts an inputted string of text.
+	echo 	-file 		Encrypts a specefied text file.
+	echo 	-save 		Saves encrypted output to a specefied file.
+	echo About
+	echo 	Written by: Joshua Ciffer
+	echo 	Version: %version%
+	echo 	Source Code: https://github.com/Joshua-Ciffer/Crypter
+	goto :eof
+
+:eof (
+	if /i "%error%"=="" (
+		exit /b 0
+	) else (
+		exit /b %error%
+	)
+)	
+endlocal
