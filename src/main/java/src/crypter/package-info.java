a6c07730ffe134c70206d168f389878307bc466b
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-03-18 17:52:19 -0400

    Setup working java directory.

---
+++ src/main/java/src/crypter/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the main code to encrypt and decrypt text that is called by the Crypter batch files.
+ *
+ * @author Joshua Ciffer
+ * @version 03/18/2018
+ */
+package src.crypter;
