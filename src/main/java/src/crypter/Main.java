278e0e7a957e46319756809463d0a90cfe5c4fa3
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-03-18 22:14:52 -0400

    Wrote main java entry point.

--- src/main/java/src/crypter/Main.java
+++ src/main/java/src/crypter/Main.java
@@ -1,81 +1,126 @@
 package src.crypter;
+import src.crypter.command.Encrypt;
+import src.crypter.command.Decrypt;
+import src.crypter.util.Command;
+import src.crypter.util.InputMode;
+import static src.crypter.util.Command.ENCRYPT;
+import static src.crypter.util.Command.DECRYPT;
+import static src.crypter.util.InputMode.STRING;
+import static src.crypter.util.InputMode.FILE;
 
 /**
  * The main entry point of the crypter java code. This will be called by Crypter.bat.
  *
- * @author Joshua
+ * @author Joshua Ciffer
  * @version 03/18/2018
  */
-final class Main {
+class Main {
 
-	private static String command;
+	/**
+	 * The crypter command to run.
+	 */
+	private static Command command;
 
-	private static String inputMode;
+	/**
+	 * The method of text input. -string will accept input from the user, -file will input from a specified file.
+	 */
+	private static InputMode inputMode;
 
+	/**
+	 * The path of the input file. Only used if -file is passed.
+	 */
 	private static String inputPath;
 
+	/**
+	 * Determines whether or not the program's output will be saved to a file.
+	 */
 	private static boolean saveOutput;
 
+	/**
+	 * The path of the output file. Only used if -save is passed.
+	 */
 	private static String outputPath;
 
+	/**
+	 * Main entry point of the encryption/decryption code.
+	 *
+	 * @param args - Command line arguments.
+	 */
 	public static void main(String[] args) {
-		System.out.println("Main.java");
-		System.out.println("args length: " + args.length);
 		for (int i = 0; i < args.length; i++) {
-			System.out.println(args[i]);
+			System.out.println("#" + i + ": " + args[i]);
 		}
 		parseCommandLineArguments(args);
+		callCommand(command);
 	}
 
+	/**
+	 * Sets program flags and variables based off of what arguments are passed from the parent batch script.
+	 * 
+	 * @param args - Command line arguments.
+	 * @throws IllegalArgumentException - Thrown if any of the command line arguments are invalid.
+	 */
 	private static void parseCommandLineArguments(String[] args) throws IllegalArgumentException {
-		if ((command.equals("encrypt") || command.equals("decrypt")) && (inputMode.equals("string") || inputMode.equals("file"))) {
-			command = args[0];
-			inputMode = args[1];
-			switch (inputMode.toLowerCase()) {
-				case "string": {
-					saveOutput = Boolean.getBoolean(args[2]);
-					switch (Boolean.toString(saveOutput)) {
-						case "true": {
-							outputPath = args[3];
-							break;
-						}
-						case "false": {
-							outputPath = null;
-							break;
-						}
-						default: {
-							throw new IllegalArgumentException("Illegal arguments.");
-						}
-
-					}
-					break;
-				}
-				case "file": {
-					inputPath = args[2];
-					saveOutput = Boolean.getBoolean(args[3]);
-					switch (Boolean.toString(saveOutput)) {
-						case "true": {
-							outputPath = args[4];
-							break;
-						}
-						case "false": {
-							outputPath = null;
-							break;
-						}
-						default: {
-							throw new IllegalArgumentException("Invalid arguments.");
-						}
-					}
-					break;
-				}
-				default: {
-					throw new IllegalArgumentException("Invalid arguments.");
-				}
-			}
-		} else {
-			throw new IllegalArgumentException("Invalid arguments.");
+		command = Command.parseCommand(args[0]);
+		if (!(command == ENCRYPT) && !(command == DECRYPT)) {
+			throw new IllegalArgumentException("The command you entered is not valid.  Please refer to the command usage.");
 		}
+		inputMode = InputMode.parseInputMode(args[1]);
+		if (!(inputMode == STRING) && !(inputMode == FILE)) {
+			throw new IllegalArgumentException("Incorrect usage of options.  Please refer to the command usage.");
+		}
+		switch (inputMode) {
+			case STRING: {
+				saveOutput = Boolean.getBoolean(args[2]);
+				switch (Boolean.toString(saveOutput)) {
+					case "true": {
+						outputPath = args[3];
+						break;
+					}
+					case "false": {
+						outputPath = null;
+						break;
+					}
+				}
+				break;
+			}
+			case FILE: {
+				inputPath = args[2];
+				saveOutput = Boolean.getBoolean(args[3]);
+				switch (Boolean.toString(saveOutput)) {
+					case "true": {
+						outputPath = args[4];
+						break;
+					}
+					case "false": {
+						outputPath = null;
+						break;
+					}
+				}
+				break;
+			}
+		}
+	}
 
+	/**
+	 * Calls the command class that was called by the crypter batch file.
+	 *
+	 * @param command - The command to call.
+	 */
+	private static void callCommand(Command command) {
+		switch (command) {
+			case ENCRYPT: {
+				Encrypt.runCommand(inputMode, inputPath, saveOutput, outputPath);
+				break;
+			}
+			case DECRYPT: {
+				Decrypt.runCommand(inputMode, inputPath, saveOutput, outputPath);
+				break;
+			}
+			default: {
+				throw new IllegalArgumentException("A command was never specified to run.");
+			}
+		}
 	}
 
 }
