f4d47c62bced311bb05416feaa8bfb958a4e4c05
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-03-18 23:14:52 -0400

    Wrote encrypt command methods to gather text input from file or user.

--- src/main/java/src/crypter/command/Decrypt.java
+++ src/main/java/src/crypter/command/Decrypt.java
@@ -1,4 +1,6 @@
 package src.crypter.command;
+import java.util.Scanner;
+import src.crypter.util.InputMode;
 
 /**
  * 
@@ -6,8 +8,49 @@ package src.crypter.command;
  * @author Joshua Ciffer
  * @version 03/18/2018
  */
-final class Decrypt {
+public class Decrypt {
 
+	private static String encryptedString;
 	
+	private static String decryptedString;
+	
+	public static void runCommand(InputMode inputMode, String inputPath, boolean saveOutput, String outputPath) {
+		switch (inputMode) {
+			case STRING: {
+				encryptedString = inputString();
+				break;
+			}
+			case FILE: {
+				encryptedString = loadFile(inputPath);
+				break;
+			}
+			default: {
+				throw new IllegalArgumentException("An input mode was not specified.");
+			}
+		}
+		decryptedString = decryptString(encryptedString);
+		if (saveOutput) {
+			
+		} else {
+			System.out.println("Decrypted output: " + decryptedString);
+		}
+	}
+
+	private static String inputString() {
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Enter a string to decrypt: ");
+		String inputtedString = userInput.nextLine();
+		userInput.close();
+		userInput = null;
+		return inputtedString;
+	}
+	
+	private static String loadFile(String filePath) {
+		return null;
+	}
+	
+	private static String decryptString(String encryptedString) {
+		return null;
+	}
 	
 }
--- src/main/java/src/crypter/command/Encrypt.java
+++ src/main/java/src/crypter/command/Encrypt.java
@@ -1,18 +1,110 @@
 package src.crypter.command;
+
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
 import src.crypter.util.InputMode;
 
 /**
- * 
+ * Command that will encrypt text inputed from different sources.
  *
- * @author Joshua
+ * @author Joshua Ciffer
  * @version 03/18/2018
  */
-class Encrypt {
+public class Encrypt {
 
-	private static InputMode inputMode;
-	
-	static String encryptString() {
-		return null;
+	/**
+	 * The text that the user provides to be encrypted.
+	 */
+	private static String unencryptedString;
+
+	/**
+	 * The encrypted form of the user's text.
+	 */
+	private static String encryptedString;
+
+	/**
+	 * Runs the command to gather text input, and then to encrypt it. The output will be printed to the user and saved to a file if the user chooses.
+	 *
+	 * @param inputMode - The method of text input. -string will accept input from the user, -file will input from a specified file.
+	 * @param inputPath - The path of the input file. Only used if -file is passed.
+	 * @param saveOutput - Determines whether or not the program's output will be saved to a file.
+	 * @param outputPath - The path of the output file. Only used if -save is passed.
+	 * @throws IllegalArgumentException - Thrown if an input mode was not specified.
+	 */
+	public static void runCommand(InputMode inputMode, String inputPath, boolean saveOutput, String outputPath) throws IllegalArgumentException {
+		switch (inputMode) {
+			case STRING: {
+				unencryptedString = inputString();
+				break;
+			}
+			case FILE: {
+				unencryptedString = loadFile(inputPath);
+				break;
+			}
+			default: {
+				throw new IllegalArgumentException("An input mode was not specified.");
+			}
+		}
+		encryptedString = encryptString(unencryptedString);
+		if (saveOutput) {
+			saveOutput(outputPath);
+			System.out.println("\nEncrypted output was saved to: " + outputPath);
+		} else {
+			System.out.println("\nEncrypted output: " + encryptedString);
+		}
+		System.exit(0);
 	}
-	
+
+	/**
+	 * Prompts the user to input a string to be encrypted.
+	 * 
+	 * @return The user's input.
+	 */
+	private static String inputString() {
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("\nEnter a string to encrypt: ");
+		String inputtedString = userInput.nextLine();
+		userInput.close();
+		userInput = null;
+		return inputtedString;
+	}
+
+	/**
+	 * Loads the text from a specified file to be encrypted.
+	 * 
+	 * @param filePath - The path of the input file.
+	 * @return The contents of the input file.
+	 */
+	private static String loadFile(String filePath) {
+		String inputtedString = "";
+		try {
+			inputtedString = new String(Files.readAllBytes(Paths.get(filePath)));
+		} catch (IOException e) {
+			System.err.println("\nThe file you specified could not be found.");
+			System.exit(2);
+		}
+		return inputtedString;
+	}
+
+	/**
+	 * Encrypts the text the user has provided.
+	 *
+	 * @param unencryptedString - The unencrypted text.
+	 * @return The encrypted text.
+	 */
+	private static String encryptString(String unencryptedString) {
+		return unencryptedString;
+	}
+
+	/**
+	 * Saves the encrypted output to a file.
+	 *
+	 * @param outputPath - The path of the output file.
+	 */
+	private static void saveOutput(String outputPath) {
+
+	}
+
 }
